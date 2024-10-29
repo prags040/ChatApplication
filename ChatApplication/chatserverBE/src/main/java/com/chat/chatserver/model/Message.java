dfa83360742448ee1fb8e86f25c8862859b35c74
Karthik Pragada <67573472+prags040@users.noreply.github.com>
2024-10-30 00:51:13 +0530

    Add files via upload
    Created an basic functioning chatApplication using Reactjs (frontend) and Java SpringBoot (Backend Server)

---
+++ ChatApplication/chatserverBE/src/main/java/com/chat/chatserver/model/Message.java
@@ -0,0 +1,13 @@
+package com.chat.chatserver.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Message {
+    private String senderName, receiverName, message, date;
+    private Status status;
+}
